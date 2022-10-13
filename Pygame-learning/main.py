306a11beb71908b329e353eea4580419d9d333c5
AccelZero <accelzero2012@gmail.com>
2022-10-13 19:43:53 +0300

    Добавил изменения в проект pygame-learning

--- Pygame-learning/main.py
+++ Pygame-learning/main.py
@@ -1,3 +1,15 @@
 import pygame
+from sys import exit
 
 pygame.init()
+screen = pygame.display.set_mode((800,400))
+game_icon = pygame.image.load("picture/game_icon.png")
+pygame.display.set_caption('My First Game')
+pygame.display.set_icon(game_icon)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    pygame.display.update()
