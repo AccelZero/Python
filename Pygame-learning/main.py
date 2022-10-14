9098c97d5fb3019b552210a673ffa4d6ccfd450d
AccelZero <accelzero2012@gmail.com>
2022-10-14 20:15:54 +0300

    Добавил изменения в проект pygame-learning

--- Pygame-learning/main.py
+++ Pygame-learning/main.py
@@ -9,15 +9,17 @@ pygame.display.set_icon(game_icon)
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("font/pico-8.ttf", 25)
 
-sky_surface = pygame.image.load("picture/graphics/sky.jpg").convert()
-ground_surface = pygame.image.load("picture/graphics/ground.png").convert()
-text_surface = test_font.render("My First Game!", False, "Black")
+sky_surf = pygame.image.load("picture/graphics/sky.jpg").convert()
+ground_surf = pygame.image.load("picture/graphics/ground.png").convert()
 
-enemy_deda_surface = pygame.image.load("picture/enemy/deda.png").convert_alpha()
-enemy_deda_rect = enemy_deda_surface.get_rect(topleft = (600, 197))
+score_surf = test_font.render("Score", False, (150, 200, 100))
+score_rect = score_surf.get_rect(center=(400, 50))
 
-player_surface = pygame.image.load("picture/player/player_1.png").convert_alpha()
-player_rect = player_surface.get_rect(midbottom = (80, 300))
+deda_surf = pygame.image.load("picture/enemy/deda.png").convert_alpha()
+deda_rect = deda_surf.get_rect(topleft=(600, 197))
+
+player_surf = pygame.image.load("picture/player/player_1.png").convert_alpha()
+player_rect = player_surf.get_rect(midbottom=(80, 300))
 
 while True:
     for event in pygame.event.get():
@@ -28,14 +30,21 @@ while True:
         #     if player_rect.collidepoint(event.pos):
         #         print("collision")
 
-    screen.blit(sky_surface, (0, 0))
-    screen.blit(ground_surface, (0, 290))
-    screen.blit(text_surface, (265, 50))
-    enemy_deda_rect.x -= 4
-    if enemy_deda_rect.right < 0:
-        enemy_deda_rect.left = 800
-    screen.blit(enemy_deda_surface, enemy_deda_rect)
-    screen.blit(player_surface, player_rect)
+    screen.blit(sky_surf, (0, 0))
+    screen.blit(ground_surf, (0, 290))
+    pygame.draw.rect(screen, "Black", score_rect)
+    pygame.draw.rect(screen, "Black", score_rect, 10)
+
+    screen.blit(score_surf, score_rect)
+    deda_rect.x -= 4
+    if deda_rect.right < 0:
+        deda_rect.left = 800
+    screen.blit(deda_surf, deda_rect)
+    screen.blit(player_surf, player_rect)
+
+    # keys = pygame.key.get_pressed()
+    # if keys[pygame.K_SPACE]:
+    #     print("jump")
 
     # if player_rect.colliderect(enemy_deda_rect):
     #     print("collision")
